e5e6da2bcf5e53bf5d7dc8ec583de77e4d375fe7
Ernest Kamau <1.kamauernest@gmail.com>
2025-05-26 11:30:33 +0300

    debugging

--- lib/scripts/run_queries.py
+++ lib/scripts/run_queries.py
@@ -15,43 +15,44 @@ def run():
     # add_author_with_articles("Adrian Githae", articles)
     
    
-    # author = Author.find_by_id(1)
+    author = Author.find_by_id(1)
     magazine = Magazine.find_by_id(1)
     article = Article.find_by_title("Data Science with Python")
     
-    # if author:
-    #     print (author.name)
+    if author:
+        print (author.name)
         
-    #     print("Magazines:")
-    #     for mag in author.magazines():
-    #         print(f"- {mag.name} ({mag.category})")
+        print("Magazines:")
+        for mag in author.magazines():
+            print(f"- {mag.name} ({mag.category})")
 
-    #     print("Topic Areas:")
-    #     print(author.topic_areas())
+        print("Topic Areas:")
+        print(author.topic_areas())
 
-    # else:
-    #     print("Author not found.")
+    else:
+        print("Author not found.")
         
     if magazine:
-        # print(magazine)
+        print(magazine)
         
         print(magazine.article_counts())
+        print(magazine.articles())
         
-    #     print(magazine.contributors())
+        print(magazine.contributors())
         
-        # print(magazine.articles())
+        print(magazine.articles())
         
-    #     print(magazine.contributing_authors())
+        print(magazine.contributing_authors())
         
-    #     print(magazine.article_titles())
+        print(magazine.article_titles())
     else:
         print("Magazine not found!")
         
-    # if article:
-    #     print(article)
-    #     print(article.author())
+    if article:
+        print(article)
+        print(article.author())
         
-    #     print(article.magazines())
+        print(article.magazines())
         
         
         
